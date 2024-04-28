bb68bb63a2476fc21889d9ed804b5da872ec9077
MrSmart00 <nexearth@gmail.com>
2024-04-28 18:56:47 +0900

    Add: workspace and project files

---
+++ Sandbox/Sandbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sandbox
+//
+//  Created by 日野森寛也 on 2024/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sandbox/Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by 日野森寛也 on 2024/04/28.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
